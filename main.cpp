6ae4040414256f1e6b77c4da2d307e1d1e24428a
Gabriel Salgado <abg.2jz1810@gmail.com>
2026-06-28 19:51:09 -0300

    feat(main): validate program arguments with exceptions

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
-int main(){
-    
+#include "src/server/Server.hpp"
+
+int main(int ac, char **av) {
+    if (ac != 3) {
+	    std::cerr << "Usage: " << av[0] << " <port> <password>" << std::endl;
+        return (1);
+    }
+    try {
+        Server s(static_cast<const std::string>(av[1]), static_cast<const std::string>(av[2]));
+    }
+    catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
