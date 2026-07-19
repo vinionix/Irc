f611f7264839d806d74c38e31c42eb6ef2765181
vinionix <viniciusfidelis289@gmail.com>
2026-07-19 12:41:22 -0300

    feat(client): add getFd method and default constructor to Client class

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -1,5 +1,13 @@
 #include "Client.hpp"
 
+int Client::getFd() const {
+    return _clientFd;
+}
+
+Client::Client() : _clientFd(-1), _isRegistered(false), _PassOk(false) {
+
+}
+
 Client::Client(int fd) : _clientFd(fd), _isRegistered(false), _PassOk(false) {
 
 }
