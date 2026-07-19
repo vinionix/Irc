034c058298b01650f08199c23799cbd7b8be3056
vinionix <viniciusfidelis289@gmail.com>
2026-07-19 12:41:08 -0300

    feat(server): implement client disconnection handling and update Channel methods

--- src/channel/Channel.cpp
+++ src/channel/Channel.cpp
@@ -1,5 +1,13 @@
 #include "Channel.hpp"
 
+bool Channel::hasClient(int fd) const {
+    return _users.find(fd) != _users.end();
+}
+
+void Channel::removeClient(int fd) {
+    _users.erase(fd);
+}
+
 Channel::~Channel() {
 
 }
