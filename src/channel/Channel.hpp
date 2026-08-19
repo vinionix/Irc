bd37cd7c789a2a5369b489656e721e87c5feb18f
vfidelis <141090825+vinionix@users.noreply.github.com>
2026-08-19 17:41:49 -0300

    style(channel): remove unused include and normalize header

--- src/channel/Channel.hpp
+++ src/channel/Channel.hpp
@@ -1,49 +1,47 @@
 #pragma once
 
 #include <iostream>
-#include <map>
-#include "../client/Client.hpp"
 #include <set>
 
 class Channel
 {
-    private:
-        std::string		_name;
-        std::string		_topic;
-        std::string		_password;
-        bool			_inviteOnly;
-        bool			_topicRestricted;
-        int				_maxUsers;
-        std::set<int>	_users;
-        std::set<int>	_operators;
+	private:
+		std::string		_name;
+		std::string		_topic;
+		std::string		_password;
+		bool			_inviteOnly;
+		bool			_topicRestricted;
+		int				_maxUsers;
+		std::set<int>	_users;
+		std::set<int>	_operators;
 		std::set<int>	_invitedUsers;
 
-    public:
-        Channel(const std::string& name);
-        ~Channel();
+	public:
+		Channel(const std::string& name);
+		~Channel();
 
-        const   std::string& getName() const;
-        const   std::string& getTopic() const;
-        void    setTopic(const std::string& topic);
-        const   std::string& getPassword() const;
-        void    setPassword(const std::string& password);
-        bool    isInviteOnly() const;
-        void    setInviteOnly(bool mode);
-        bool    isTopicRestricted() const;
-        void    setTopicRestricted(bool mode);
-        int     getMaxUsers() const;
-        void    setMaxUsers(int limit);
+		const std::string&	getName() const;
+		const std::string&	getTopic() const;
+		void				setTopic(const std::string& topic);
+		const std::string&	getPassword() const;
+		void				setPassword(const std::string& password);
+		bool				isInviteOnly() const;
+		void				setInviteOnly(bool mode);
+		bool				isTopicRestricted() const;
+		void				setTopicRestricted(bool mode);
+		int					getMaxUsers() const;
+		void				setMaxUsers(int limit);
 
-        void    addClient(int fd);
-        void    removeClient(int fd);
-        bool    hasClient(int fd) const;
-        const   std::set<int>& getUsers() const;
+		void				addClient(int fd);
+		void				removeClient(int fd);
+		bool				hasClient(int fd) const;
+		const std::set<int>&	getUsers() const;
 
-        void    addOperator(int fd);
-        void    removeOperator(int fd);
-        bool    isOperator(int fd) const;
+		void				addOperator(int fd);
+		void				removeOperator(int fd);
+		bool				isOperator(int fd) const;
 
-        void    addInvite(int fd);
-        void    removeInvite(int fd);
-        bool    isInvited(int fd) const;
+		void				addInvite(int fd);
+		void				removeInvite(int fd);
+		bool				isInvited(int fd) const;
 };
